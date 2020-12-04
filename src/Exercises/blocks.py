73f70b3c731505bc28abc645ace510a0ba493412
Brian Barton <brian.n.barton@gmail.com>
2020-12-04 09:40:37 -0700

    Elif, and debugging

--- src/Exercises/blocks.py
+++ src/Exercises/blocks.py
@@ -1,11 +1,17 @@
 
+
+
 name = input("please enter your name: ")
 age = int(input("how old are you {0}? ".format(name)))
 print(age)
-if age >= 18:
-    print("you are old enough to vote")
+
+if age < 18:
+    print("please come back in {0} years".format(18 - age))
+elif age == 900:
+    print("Whadup Yoda")
 else:
-    print("please come back in {0} years",format(18 - age))
+    print("you are old enough to vote")
+    print("please put an x in the box ")
 
 #
 # for i in range(1, 13):
@@ -14,7 +20,7 @@ else:
 
 
 
-# I found a list of parts needed to build an affordable and styleish coffee table .
+# I found a list of parts needed to build an affordable and styleish coffee table.
 # I needed to calculate the total cost
 
 
