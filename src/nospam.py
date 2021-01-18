0e972452dd0c55de31ce3a26d6d6db69d5b2513d
Brian Barton <brian.n.barton@gmail.com>
2021-01-18 13:01:43 -0700

    second solotuion to remove an item from a list

--- src/nospam.py
+++ src/nospam.py
@@ -10,10 +10,15 @@ menu = [
 ]
 
 
+# for meal in menu:
+#     for index in range(len(meal) -1, -1, -1):
+#         if meal[index] == "spam":
+#             del meal[index]
+#
+#         print(meal)
+
 for meal in menu:
-    for index in range(len(meal) -1, -1, -1 ):
-        if meal[index] == "spam":
-            del meal[index]
-
-        print(meal)
-
+    for item in meal:
+        if item != "spam":
+            print(item)
+    print()
